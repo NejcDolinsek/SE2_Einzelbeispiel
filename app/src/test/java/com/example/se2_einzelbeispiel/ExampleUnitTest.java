d595ae10010f24e107630f8f6d8e1122bdfe9f67
NejcDolinsek <76431204+NejcDolinsek@users.noreply.github.com>
2022-03-11 11:00:32 +0100

    Wrote Tests to test the Methods in class OrderAndDeletePrimeNumbers

--- app/src/test/java/com/example/se2_einzelbeispiel/ExampleUnitTest.java
+++ app/src/test/java/com/example/se2_einzelbeispiel/ExampleUnitTest.java
@@ -1,17 +1,41 @@
 package com.example.se2_einzelbeispiel;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+import android.widget.EditText;
+
 public class ExampleUnitTest {
+
+    String Matrikelnummer;
+    int[] MatrikelnummerArray;
+
+    @Before
+    public void setup(){
+        Matrikelnummer = "11704726";
+        MatrikelnummerArray = new int[]{1, 1, 7, 0, 4, 7, 2, 6};
+    }
+    @After
+    public void clear(){
+        Matrikelnummer = null;
+        MatrikelnummerArray = null;
+    }
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void TestIsPrime(){
+        assertTrue(OrderAndDeletePrimeNumbers.isPrime(7));
+    }
+    @Test
+    public void StringToArray(){
+        int [] ArrayMatrikelnummer = {1,1,7,0,4,7,2,6};
+        assertArrayEquals(ArrayMatrikelnummer,OrderAndDeletePrimeNumbers.StringToArray(Matrikelnummer));
+    }
+    @Test
+    public void ArraySorter(){
+        int [] ArraySortedMatrikelnummer = {0,1,1,2,4,6,7,7};
+        assertArrayEquals(ArraySortedMatrikelnummer,MatrikelnummerArray);
+
     }
 }
